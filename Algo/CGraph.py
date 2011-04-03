b5faec00fe704246d1006104f50412a425ef61ed
Balthazar Rouberol <balto@Balto-Latitude.(none)>
2011-04-03 16:07:25 +0200

    Genome2adj, fitness, GraphPrint

--- Algo/CGraph.py
+++ Algo/CGraph.py
@@ -8,8 +8,6 @@ class CGraph:
     def __init__(self,nbNodes):
         self.nbNodes = nbNodes
         
-        A = zeros(shape=(self.nbNodes,self.nbNodes))
-
         ThDegres = range(1,self.nbNodes) # Distribution theorique
         ThDegres.reverse()
         ThDistrib = []
@@ -17,7 +15,8 @@ class CGraph:
         for deg in ThDegres:
             ThDistrib.append(self.nbNodes*deg**(-2.2))
 
-        ''' On prend les degres dans le sens decroissant et on calcul l'effectif theorique correspondant a ce degre. Ensuite, on attribut a chaque noeuds un degre theorique (biais scale_free) '''
+        ''' On prend les degres dans le sens decroissant et on calcul l'effectif theorique 
+        correspondant a ce degre. Ensuite, on attribut a chaque noeuds un degre theorique (biais scale_free) '''
         N=0
         res=[-1]*self.nbNodes
         while N<self.nbNodes:
@@ -26,43 +25,25 @@ class CGraph:
                 i+=1
             res[N]=ThDegres[i]
             ThDistrib = [t-1 for t in ThDistrib]
-            N+=1
-            distri=res
-            
+            N+=1          
+        distri = res
 
-        """ Remplissage et creation de la matrice d'adjacence """
-        Adj = zeros(shape=(self.nbNodes,self.nbNodes))
-        print distri
-
-        for i in (range(self.nbNodes-1)):
+        """ Creation du genome """ 
+        self.genome = []
+        for i in (range(self.nbNodes)):
             li=[0]*(self.nbNodes-i-1)
-            for k in range(distri[i]):
-                li[k]=1
+            if i != self.nbNodes-1:
+                for k in range(distri[i]):
+                    li[k]=1
             random.shuffle(li)
             li.insert(0,0)
-            print "ligne : "+str(li)
-
-            for j in range(i,self.nbNodes-1):
-                Adj[i,j] = A[j,i] = li[j-i]
-            
-
-                               
-
-        """ Creation du graphe a partir de la matrice d'adjacence """	
-        self.graph = nx.Graph()
-
-        for i in range (self.nbNodes):
-            self.graph.add_node(i)		#Ajout de tous les noeuds
-            for j in range(self.nbNodes):
-                if i<j :		#Pour n'ajouter que la partie superieure de la matrice
-                    if A[i,j]==1:	
-                        self.graph.add_edge(i,j)	#Ajout des aretes
+            self.genome += li
 
 
 
-    """ Calcul du coefficient de correlation entre distribution theorique et experimentale 
-    Compris entre 0 et 1 """
     def fit2distri(self):
+        """ Calcul du coefficient de correlation entre distribution theorique et experimentale 
+    Compris entre 0 et 1 """
 
         ThDistrib = [] # Distribution theorique
         
@@ -75,20 +56,20 @@ class CGraph:
             res += d/(d+ThDistrib[i])            
 
         # TMP
-        # Deg = self.graph.degree().keys()
-        # Val = self.graph.degree().values()
-        # plt.figure()
-        # plt.plot(Deg[1:],Val[1:],'ro-') #in-degree
-        # plt.xlabel('Degree')
-        # plt.ylabel('Number of nodes')
-        # plt.title('Degree distribution')
+        Deg = self.graph.degree().keys()
+        Val = self.graph.degree().values()
+        plt.figure()
+        plt.plot(Deg[1:],Val[1:],'ro-') #in-degree
+        plt.xlabel('Degree')
+        plt.ylabel('Number of nodes')
+        plt.title('Degree distribution')
 
-        # for x in Deg:
-        #         if x!=0:
-        #                 plt.scatter(x,self.nbNodes*x**(-2.2),c="blue")
+        for x in Deg:
+                if x!=0:
+                        plt.scatter(x,self.nbNodes*x**(-2.2),c="blue")
 
-        # plt.savefig('DegreeDistrib.png')
-        # plt.close()
+        plt.savefig('DegreeDistrib.png')
+        plt.close()
 
         # ATTENTION : lorsqu'on converge vers un scale-free: queue de distrib
         # tendant vers 0 : avg_res tend vers 1 !
@@ -99,11 +80,50 @@ class CGraph:
 
     def clustering(self):
         """ Renvoie le coeff de clustering moyen """
-        self.cluster = nx.average_clustering(self.graph)
-        
+        return nx.average_clustering(self.graph)
+
+    def smallWorld(self):
+        """ Renvoie le plus court chemin moyen entre deux noeuds """
+        # On vise une valeur de 2.5
+        return abs((nx.average_shortest_path_length(self.graph)-2.5)/(nx.average_shortest_path_length(self.graph)))
+                                                                  
+    def fitness(self,a,b,c):
+        return (a*self.fit2distrib() + b*self.clustering() + c*self.smallWorld())/(a+b+c)
+
+    def genome2adj(self):
+         """ Remplissage et creation de la matrice d'adjacence """
+         Adj = zeros(shape=(self.nbNodes,self.nbNodes))
+         i = 0
+         N = self.nbNodes
+
+         while i < len(self.genome):
+             for j in range(N):
+                 Adj[self.nbNodes - N, self.nbNodes - N +j] = Adj[self.nbNodes - N +j,self.nbNodes-N] = self.genome[i+j]
+             i+=N
+             N-=1
+
+         return Adj
+
+    def drawGraph(self): 
+        """ Creation du graphe a partir de la matrice d'adjacence """	
+        self.graph = nx.Graph()
+        Adj = self.genome2adj()
+
+        for i in range (self.nbNodes):
+            self.graph.add_node(i)		# Ajout de tous les noeuds
+            for j in range(self.nbNodes):
+                if i<j :		# Pour n'ajouter que la partie superieure de la matrice
+                    if Adj[i,j]==1:	
+                        self.graph.add_edge(i,j)	# Ajout des aretes
+
+        nx.draw(self.graph)
+        plt.savefig("Graph.png")
+
+
         
 
 
-G = CGraph(20)
+G = CGraph(50)
+G.drawGraph()
 G.fit2distri()
 G.clustering()
