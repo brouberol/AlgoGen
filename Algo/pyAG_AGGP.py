934e4531cdde368c1abe2535f7cea00430de52c0
Balthazar Rouberol <balto@Balto-Latitude.(none)>
2011-04-06 12:42:14 +0200

    pyAG_AGGP en debug.

--- Algo/pyAG_AGGP.py
+++ Algo/pyAG_AGGP.py
@@ -3,29 +3,10 @@ import networkx as nx
 from CGraph import *
 
 
-# class te:
-#     def __init__(self,ge=None):
-# 	if ge is None :
-# 	    self.genome=[randint(0,1) for x in range(2000)]
-# 	else:
-# 	    self.genome=ge[:]
-#     def fitness(self):
-# 	r=0
-# 	g = 0
-# 	for i,x in enumerate(self.genome):
-# 	    r+= 2*x-1
-# 	    if i>1000:
-# 		if r>4 or r<-4:
-# 		    g+=1
-# 	return g
-
-
-
-
-
 class pyAG:
     def __init__(self,N,prod,nodes,txMut,txCross,fita,fitb,fitc):
 	self.N=N # Nombre d'individus par population
+        self.nodes = nodes
 	self.txMut=txMut # Taux de mutation
 	self.txCross=txCross # Taux de crossover
 	self.prod = prod # individu (graphe)
@@ -35,9 +16,10 @@ class pyAG:
         self.fita=fita # 
         self.fitb=fitb # Parametres pour la somme de fitness
         self.fitc=fitc #
-        self.fitness=[-1]*self.N
+        self.fitness=[]
 
 	
+        # A MON AVIS, LE BUG EST DANS NEWPOP
     def new_pop(self):
         #if self.newpop is not None:
             #for i in range(len(self.newpop)):
@@ -47,24 +29,25 @@ class pyAG:
 	for i in range(self.N):
 	    r = randint(0,(self.N+1)*(self.N)/2)
 	    x = Index(self.N,r)
-	    ## print x,self.fitness[x][0]
-	    self.newpop.append(self.prod(self.pop[self.fitness[x][1]].genome)) 
+	    # print x,self.fitness[x][0]
+	    self.newpop.append(self.prod(self.nodes,self.pop[self.fitness[x][1]].genome)) 
+
 
     def mutation(self):
 	for graph in self.newpop:
 	    gen = graph.genome
 	    for i in range(len(gen)):
-		if random()<self.txMut:
+		if random.random()<self.txMut:
 		    gen[i]=1-gen[i]
 	    graph.genome=gen
 
     def cross(self):
 	for x in self.newpop:
-	    if random()<self.txCross:
+	    if random.random() < self.txCross:
 		g = x.genome
 		r1= self.pop[randint(0,self.N-1)].genome
-		z= randint(0,len(g)-1)
-		if random()<0.5:
+		z= random.randint(0,len(g)-1)
+		if random.random() < 0.5:
 		    g[0:z]=r1[0:z]
 		else:
 		    g[z:]=r1[z:]
@@ -72,29 +55,39 @@ class pyAG:
 
     def update(self):
 	self.pop=self.newpop[:]
-        # Attention a l'operateur de copie
-        # Delete dans newpop
-        # Il faudra supprimer chaque elt de pop et copier
 
     def calc_fitness(self):
+        self.fitness = []
         for i in range(self.N):
-            self.fitness[i]=(self.pop[i].fitness(self.fita,self.fitb,self.fitc),i)
+            self.fitness.append((self.pop[i].fitness(self.fita,self.fitb,self.fitc),i))
             self.fitness.sort()
-
+        #print self.fitness
 
 
     def fitmin(self):
-        return self.fitness[0]
+        return self.fitness[0][0]
+
+    def fitmax(self):
+        return self.fitness[self.N-1][0]
+
+    def fitmean(self):
+        S = 0
+        for i in self.fitness:
+            S += i[0]
+        res = S/len(self.fitness)
+        return res
         
     def genloop(self):
-	ga.calc_fitness()
+	self.calc_fitness()
+        #for i in self.fitness:
+            #print i
+        #print ""
 	self.new_pop()
 	self.mutation()
 	self.cross()
 	self.update()
 	self.gen += 1
-	print self.fitm,self.fim # Fitness max - Fitness min ? A VERIFIER
-
+	print "Generation {3},  FitMin = {0:4}, FitMean = {1:4}, FitMax = {2:4}".format(self.fitmin(),self.fitmean(),self.fitmax(),self.gen) 
 
 def Index(N,x):
     ind = 0
@@ -107,25 +100,25 @@ def Index(N,x):
 
 # MAIN----------------------
 
-seed(11)
+seed(14)
 
-nbGraph = 50
+nbGraph = 10
 
 # A ADAPTER A VOS BESOINS !
 txMut = 0.05
 txCrossOver = 0.1
 nbNodes = 50
-a = 0.1
+a = 1
 b = 0.2
-c = 0.3
+c = 0.1
 
 
-
+plop = 0
 AlgoGen = pyAG(nbGraph,CGraph,nbNodes,txMut,txCrossOver,a,b,c)
-AlgoGen.calc_fitness()
+#AlgoGen.calc_fitness()
 
-# for i in range(1000):
-#     ga.genloop()
+for i in range(1000):
+    AlgoGen.genloop()
 
 # r=0
 # f=open("btr2.dat","w")
@@ -135,4 +128,3 @@ AlgoGen.calc_fitness()
 # f.close()
 
 
-# BOUCLE 
